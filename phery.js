9d82aa2e6a0f42159878e2b14c6ce3f7101a471b
Paulo Cesar <pocesar@users.noreply.github.com>
2016-06-28 20:35:47 -0300

    Update phery.js

--- phery.js
+++ phery.js
@@ -82,7 +82,7 @@
 			},
 			/**
 			 * @class
-			 * @version 2.7.2
+			 * @version 2.7.4
 			 * @extends {jQuery}
 			 */
 			phery = (function(){ return function(){ return phery; }; })();
@@ -108,7 +108,7 @@
 		 *
 		 * @type {String}
 		 */
-		phery.version = '2.7.2';
+		phery.version = '2.7.4';
 
 		/**
 		 * @lends {Object.prototype.hasOwnProperty}
